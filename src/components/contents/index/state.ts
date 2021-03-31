57df47e28d51d9e05568e1d694cd4bcfd9366406
Takao Mizuno <takao.mizuno110@gmail.com>
2021-03-31 23:33:25 +0900

    added recoil-persist

--- src/components/contents/index/state.ts
+++ src/components/contents/index/state.ts
@@ -1,9 +1,13 @@
 import {atom} from 'recoil'
+import {recoilPersist} from 'recoil-persist'
 
 import {Document} from 'lib/types/documents'
 
 
+const {persistAtom} = recoilPersist()
+
 export const documentsState = atom<Document[]>({
   key: 'documentsState',
-  default: []
+  default: [],
+  effects_UNSTABLE: [persistAtom],
 })
